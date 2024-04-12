971b53c4d27ebdf2be7b3142d6aeba8cb79b2523
Aniket Raj <133547782+ANIKETRAJ28@users.noreply.github.com>
2024-04-12 18:14:03 +0530

    Create 06_AnyInTS.ts

---
+++ 06_AnyInTS.ts
@@ -0,0 +1,8 @@
+/**
+ * any type object
+ * if you want to declare the variable as type any, then mention ":any" or
+ * donot assign a value to the variable
+ */
+let x; // this variable is of type any 
+x = "10";
+x = 10;
